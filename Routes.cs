7dd8819a0955d7f4cabd7db8d431953f8234b76c
Piedone <piedone@pyrocenter.hu>
2012-08-28 12:59:10 +0200

    Usings clean-up

--- Routes.cs
+++ Routes.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Orchard.Mvc.Routes;
-using System.Web.Routing;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
 
 namespace Associativy.WebServices
 {
